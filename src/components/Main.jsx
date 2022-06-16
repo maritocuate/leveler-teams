c4019bb3278a873d55d620b6a719ff8a37fc3aa0
marioq <mario300@gmail.com>
2022-06-16 00:01:10 -0300

    setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+// rsf
+import Constants from 'expo-constants'
+import { Text, View } from 'react-native'
+
+export default function Main() {
+  return (
+    <View style={{ marginTop:Constants.statusBarHeight }}>
+      <Text>Leveler App</Text>
+    </View>
+  )
+}
